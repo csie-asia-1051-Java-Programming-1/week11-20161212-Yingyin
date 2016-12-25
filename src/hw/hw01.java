10e11a71a179ef449496adfb1fe998bcbbdb60fc
Yingyin <105791120@live.asia.edu.tw>
2016-12-25 18:00:53 +0800

    hw01 105791120 杨钰莹

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -1,10 +1,37 @@
 package hw;
+
+import java.util.Scanner;
+
 /*
  * Date: 2016/12/12
  * Author: 105021***  ???
  */
 public class hw01 {
 	public static void main(String[] args) {
-
+		Scanner scn =new Scanner(System.in);
+		System.out.println("請輸入文字: ");
+		String str = scn.nextLine();
+		System.out.println("請輸入要加的數字: ");
+		int a = scn.nextInt();
+		char data[]=str.toCharArray();
+		for(int i=0;i<data.length;i++){
+			if(data[i]>64&&data[i]<91){
+				if((int)data[i]+a>90){
+					data[i]=(char)((int)data[i]+a-26);
+				}else{
+					data[i]=(char)((int)data[i]+a);
+				}
+			}else if(data[i]>96&&data[i]<123){
+				if((int)data[i]+a>122){
+					data[i]=(char)((int)data[i]+a-26);
+				}else{
+					data[i]=(char)((int)data[i]+a);
+				}
+			}else{
+				data[i]=data[i];
+			}
+		}
+		String text = new String(data);
+		System.out.println(" 轉換成: "+text);
 	}
 }
